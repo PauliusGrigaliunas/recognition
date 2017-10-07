624df65589bdb44716af253b638fd019ea38eb33
Paulius <31667985+PauliusGrigaliunas@users.noreply.github.com>
2017-10-07 23:12:06 +0300

    update

---
+++ Recognition/Recognition/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Recognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ibOriginal = new Emgu.CV.UI.ImageBox();
+            this.ibProcessed = new Emgu.CV.UI.ImageBox();
+            this.btnPauseOrResume = new System.Windows.Forms.Button();
+            this.txtXYradius = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ibOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibProcessed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ibOriginal
+            // 
+            this.ibOriginal.Location = new System.Drawing.Point(26, 36);
+            this.ibOriginal.Name = "ibOriginal";
+            this.ibOriginal.Size = new System.Drawing.Size(640, 480);
+            this.ibOriginal.TabIndex = 2;
+            this.ibOriginal.TabStop = false;
+            // 
+            // ibProcessed
+            // 
+            this.ibProcessed.Location = new System.Drawing.Point(672, 36);
+            this.ibProcessed.Name = "ibProcessed";
+            this.ibProcessed.Size = new System.Drawing.Size(640, 480);
+            this.ibProcessed.TabIndex = 2;
+            this.ibProcessed.TabStop = false;
+            // 
+            // btnPauseOrResume
+            // 
+            this.btnPauseOrResume.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.btnPauseOrResume.Location = new System.Drawing.Point(26, 522);
+            this.btnPauseOrResume.Name = "btnPauseOrResume";
+            this.btnPauseOrResume.Size = new System.Drawing.Size(273, 159);
+            this.btnPauseOrResume.TabIndex = 3;
+            this.btnPauseOrResume.Text = "Pause";
+            this.btnPauseOrResume.UseVisualStyleBackColor = true;
+            this.btnPauseOrResume.Click += new System.EventHandler(this.PauseOrResume_Click);
+            // 
+            // txtXYradius
+            // 
+            this.txtXYradius.Location = new System.Drawing.Point(305, 522);
+            this.txtXYradius.Multiline = true;
+            this.txtXYradius.Name = "txtXYradius";
+            this.txtXYradius.ReadOnly = true;
+            this.txtXYradius.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtXYradius.Size = new System.Drawing.Size(1006, 159);
+            this.txtXYradius.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1323, 693);
+            this.Controls.Add(this.txtXYradius);
+            this.Controls.Add(this.btnPauseOrResume);
+            this.Controls.Add(this.ibProcessed);
+            this.Controls.Add(this.ibOriginal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ibOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibProcessed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox ibOriginal;
+        private Emgu.CV.UI.ImageBox ibProcessed;
+        private System.Windows.Forms.Button btnPauseOrResume;
+        private System.Windows.Forms.TextBox txtXYradius;
+    }
+}
+
---
+++ Recognition/Recognition/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+
+namespace Recognition
+{
+    public partial class Form1 : Form
+    {
+
+        VideoCapture webCam;
+        bool blnCapturingInProgress = false;
+        Image<Bgr, byte> imgOrginal;
+        Image<Gray, byte> imgProcessed;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                webCam = new VideoCapture();
+            }
+            catch (NoNullAllowedException except)
+            {
+                txtXYradius.Text = except.Message;
+            }
+
+            Application.Idle += ProcessUpdate;
+            blnCapturingInProgress = true;
+        }
+
+        private void ProcessUpdate(object sender, EventArgs e)
+        {
+            imgOrginal = webCam.QueryFrame().ToImage <Bgr,byte>();
+            if (imgOrginal == null) return;
+            imgProcessed = imgOrginal.InRange(new Bgr(0,0,0), new Bgr(100, 100, 100));
+            imgProcessed = imgProcessed.SmoothGaussian(9);
+            CircleF[] circles = imgProcessed.HoughCircles(new Gray(100),
+                new Gray(50),
+                2,
+                imgProcessed.Height / 4,
+                10,
+                400 )[0];
+
+            foreach (CircleF circle in circles) {
+                if (txtXYradius.Text != "") txtXYradius.AppendText(Environment.NewLine);
+                txtXYradius.AppendText("ball position = x" + circle.Center.X.ToString().PadLeft(4) + ", y" + circle.Center.Y.ToString().PadLeft(4) + ", radius =" +
+                    circle.Radius.ToString("###,000").PadLeft(7));
+                txtXYradius.ScrollToCaret();
+
+                CvInvoke.Circle(imgOrginal, 
+                    new Point(((int)circle.Center.X), (int)circle.Center.Y), 3, new MCvScalar(0,255,0), 
+                    -1,
+                    LineType.AntiAlias,
+                    0);
+                imgOrginal.Draw(circle, new Bgr(Color.Red), 3);
+            }
+
+            ibOriginal.Image = imgOrginal;
+            ibProcessed.Image = imgProcessed;
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (webCam != null)
+            {
+                webCam.Dispose();
+            }
+
+        }
+
+        private void PauseOrResume_Click(object sender, EventArgs e)
+        {
+            if (blnCapturingInProgress == true)
+            {
+                Application.Idle -= ProcessUpdate;
+                blnCapturingInProgress = false;
+                btnPauseOrResume.Text = "resume"; 
+            }
+            else
+            {
+                Application.Idle += ProcessUpdate;
+                blnCapturingInProgress = true;
+                btnPauseOrResume.Text = "pause";
+            }
+        }
+    }
+}
